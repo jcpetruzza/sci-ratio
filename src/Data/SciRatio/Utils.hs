b87909b10f3eb82f4235a03d9c28fe27109ca256
Phil Ruffwind <rf@rufflewind.com>
2014-08-09 04:51:00 -0400

    Simplify Utils a bit

--- src/Data/SciRatio/Utils.hs
+++ src/Data/SciRatio/Utils.hs
@@ -3,14 +3,18 @@ Stability: experimental
 
 -}
 module Data.SciRatio.Utils where
-import Control.Monad (ap)
-import Data.Functor ((<$), (<$>))
-import Data.Char (isAlpha, isDigit)
+import Control.Monad (ap, mzero)
+import Data.Char (isAlpha, isDigit, toLower)
 import Data.Ratio (denominator, numerator)
 import Data.SciRatio (SciRatio, expPart, ratioPart)
 import Numeric (readDec, readHex, readInt, readOct, showIntAtBase)
 import Text.ParserCombinators.ReadP (ReadP, (<++))
 import qualified Text.ParserCombinators.ReadP as P
+
+-- | Whether the string begins with a prefix indicating its radix (e.g. @0x@).
+hasRadixPrefix :: String -> Bool
+hasRadixPrefix ('0' : c : _) = isAlpha c
+hasRadixPrefix _             = False
 
 -- | Convert a binary literal into a positive number.  The result is
 --   undefined if the input is not a valid binary literal.
@@ -19,61 +23,50 @@ readBin = readInt 2 isBinDigit digitToInt
   where digitToInt c = fromEnum c - fromEnum '0'
 
 -- | Convert a positive number into its binary string representation.
-showBin :: (Integral a, Show a) => a -> String
-showBin x = showIntAtBase 2 intToDigit x ""
+showBin :: (Integral a, Show a) => a -> ShowS
+showBin x = showIntAtBase 2 intToDigit x
   where intToDigit = toEnum . (+ fromEnum '0') . fromIntegral
 
--- | Parse a binary digit (either @\'0\'@ or @\'1\'@).
+-- | Whether the character is a binary digit (either @\'0\'@ or @\'1\'@).
 isBinDigit :: Char -> Bool
 isBinDigit c = fromEnum '0' <= x && x <= fromEnum '1' where x = fromEnum c
 
-hasRadixPrefix :: String -> Bool
-hasRadixPrefix ('0' : c : _) = isAlpha c
-hasRadixPrefix _             = False
+-- | Whether the character can be used as an exponent symbol (@\'e\'@,
+--   @\'E\'@, or @\'\\x23e8\'@) in scientific notation.
+isDecimalExponentSymbol :: Char -> Bool
+isDecimalExponentSymbol = (`elem` "eE\x23e8")
 
--- | A decimal point (@\'.\'@).
-pDecimalPoint :: ReadP ()
-pDecimalPoint = () <$ P.char '.'
-
--- | Parse an exponent symbol (@\'e\'@, @\'E\'@, or @\'\\x23e8\'@) in
---   scientific notation.
-pDecimalExponentSymbol :: ReadP ()
-pDecimalExponentSymbol = () <$ P.satisfy (`elem` "eE\x23e8")
-
--- | Parse a fraction separator (@\'/\'@ or @\'\\x2044\'@).
-pFractionSeparator :: ReadP ()
-pFractionSeparator = () <$ P.satisfy (`elem` "/\x2044")
-
--- | Parse a decimal digit (from @\'0\'@ to @\'9\'@).
-pDigit :: ReadP Char
-pDigit = P.satisfy isDigit
+-- | Whether the character is a fraction separator (@\'/\'@ or @\'\\x2044\'@).
+isFractionSeparator :: Char -> Bool
+isFractionSeparator = (`elem` "/\x2044")
 
 -- | Parse a number prefixed by an optional sign.  The Unicode minus sign is
 --   accepted.
 pSigned :: Num a =>
            ReadP a                      -- ^ Parser for the number.
         -> ReadP a
-pSigned p = fmap (*) (sign <++ return 1) `ap` p
-  where sign = do
-          c <- P.get
-          case c of
-            '+'      -> return   1
-            '-'      -> return (-1)
-            '\x2212' -> return (-1)
-            _        -> P.pfail
+pSigned p = do
+  sign <- (<++ return 1) $ do
+    c <- P.get
+    case c of
+      '+'      -> return   1
+      '-'      -> return (-1)
+      '\x2212' -> return (-1)
+      _        -> mzero
+  num <- p
+  return (sign * num)
 
--- | Parse a natural number (unsigned integer) of arbitrary size.  Accepts a
---   nonempty string of 'pDigit's.
+-- | Parse a natural number (unsigned integer) of arbitrary size in decimal
+--   form.  Accepts a nonempty string of digits.
 pNatural :: Num a => ReadP a
-pNatural = fromInteger <$> P.readS_to_P readDec
+pNatural = fromInteger `fmap` P.readS_to_P readDec
 
--- | Parse an integer (signed).  Accepts a 'pSigned' 'pNatural'.
+-- | Parse an (signed) integer.
 pInteger :: Num a => ReadP a
 pInteger = pSigned pNatural
 
--- | Parse an unsigned decimal fraction.  Accepts a nonempty string of
---   'pDigit's with a single optional 'pDecimalPoint' placed anywhere in the
---   string.
+-- | Parse an unsigned decimal fraction.  Accepts a nonempty string of digits
+--   with a single optional decimal point placed anywhere in the string.
 pUnsignedDecimal :: Fractional a => ReadP a
 pUnsignedDecimal = do
   intPart  <- P.munch isDigit
@@ -93,58 +86,45 @@ pUnsignedDecimal = do
   where readInt' "" = 0
         readInt' s  = fromInteger (read s)
 
--- | Parse a (signed) decimal fraction.  Accepts a 'pSigned'
---   'pUnsignedDecimal'.
+-- | Parse a (signed) decimal fraction.
 pDecimal :: Fractional a => ReadP a
 pDecimal = pSigned pUnsignedDecimal
 
--- | Parse an exponent in scientific notation.  Accepts a
---   'pDecimalExponentSymbol' followed by an 'pInteger'.
+-- | Parse an exponent in scientific notation.  Accepts a decimal exponent
+--   symbol followed by an integer.
 pSciExponent :: Num a => ReadP a
-pSciExponent = pDecimalExponentSymbol >> pInteger
+pSciExponent = P.satisfy isDecimalExponentSymbol >> pInteger
 
--- | Parse an unsigned scientific decimal fraction.  Equivalent to
---   an 'pUnsignedDecimal' optionally followed by a 'pSciExponent'.
+-- | Parse an unsigned scientific decimal fraction.
 pUnsignedSciDecimal :: Fractional a => ReadP a
 pUnsignedSciDecimal =
   fmap (*) pDecimal `ap` fmap pow10 (pSciExponent <++ return 0)
   where pow10 = (10 ^^) :: Fractional a => Integer -> a
 
--- | Parse a (signed) scientific decimal fraction.  Accepts a 'pSigned'
---   'pUnsignedSciDecimal'.
+-- | Parse a (signed) scientific decimal fraction.
 pSciDecimal :: Fractional a => ReadP a
 pSciDecimal = pSigned pUnsignedSciDecimal
 
--- | Parse a ratio of two numbers.
+-- | Parse a ratio of two numbers, each of which is parsed with the given
+--   parser.
 pRatio :: Fractional a =>
           ReadP a               -- ^ Parser for the numerator and denominator.
        -> ReadP a
-pRatio p = fmap (/) p `ap` ((pFractionSeparator >> p) <++ return 1)
-
--- | Parse a 'pRatio' of 'pInteger's.
-pIntegerRatio :: Fractional a => ReadP a
-pIntegerRatio = pRatio pInteger
-
--- | Parse a 'pRatio' of 'pDecimal's.
-pDecimalRatio :: Fractional a => ReadP a
-pDecimalRatio = pRatio pDecimal
-
--- | Parse a 'pRatio' of 'pSciDecimal's.
-pSciDecimalRatio :: Fractional a => ReadP a
-pSciDecimalRatio = pRatio pSciDecimal
+pRatio p = fmap (/) p `ap` ((P.satisfy isFractionSeparator >> p) <++ return 1)
 
 -- | Parse a natural number in either binary (@0b@), octal (@0o@), or
---   hexadecimal (@0x@) format (but not decimal).  Uppercased prefixes are
---   allowed as well.
+--   hexadecimal (@0x@) format (but not decimal).  The prefix is not
+--   case-sensitive.
 pNaturalBOH_ :: Num a => ReadP a
 pNaturalBOH_ =
-  fromInteger <$> P.choice
-  [ P.string "0x" >> P.readS_to_P readHex
-  , P.string "0o" >> P.readS_to_P readOct
-  , P.string "0b" >> P.readS_to_P readBin ]
+  P.char '0' >> fromInteger `fmap` P.choice
+  [ char' 'x' >> P.readS_to_P readHex
+  , char' 'o' >> P.readS_to_P readOct
+  , char' 'b' >> P.readS_to_P readBin ]
+  where char' x = P.satisfy $ \ c -> toLower c == toLower x
 
 -- | Parse a natural number in either binary, octal, decimal, or hexadecimal
---   format.  See 'pNaturalBOH_' for details on accepted formats.
+--   format.  See @'pNaturalBOH_'@ for details on accepted formats.
 pNaturalBOH :: Num a => ReadP a
 pNaturalBOH = do
   string <- P.look
@@ -152,16 +132,16 @@ pNaturalBOH = do
     then pNaturalBOH_
     else pNatural
 
--- | Parse the most general number format: either 'naturalBOH_' or
---   'pSciDecimalRatio'.
+-- | Parse the most general number format: either @'naturalBOH_'@ or
+--   @'pRatio pSciDecimal'@.
 pNumber :: Fractional a => ReadP a
 pNumber = do
   string <- P.look
   if hasRadixPrefix string
     then pNaturalBOH_
-    else pSciDecimalRatio
+    else pRatio pSciDecimal
 
--- | Parse the string using the 'pNumber' parser.
+-- | Parse the string using the @'pNumber'@ parser.
 parseNumber :: (Fractional a, Real a, Integral b) =>
                String -> Maybe (SciRatio a b)
 parseNumber s = case P.readP_to_S pNumber s of
